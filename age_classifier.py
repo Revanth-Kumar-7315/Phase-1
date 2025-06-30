150e8cdc308a2fd73f97212dece5381c6d47171e
Revanth Kumar <revanth07315@gmail.com>
2025-06-30 23:14:07 +0530

    Day 1 – Added calculator, even_odd, age_classifier scripts

---
+++ age_classifier.py
@@ -0,0 +1,8 @@
+Age = int(input("Enter your age :"))
+
+if Age < 12:
+    print("You are a Child.")
+if 12 < Age < 18:
+    print("You are a Teenager.")
+else:
+    print("You are an Adult.")
---
+++ calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number :"))
+b = int(input("Enter a number :"))
+
+print("Sum :-",a+b)
+print("Difference :-",a-b)
+print("Product :-",a*b)
+
+if b != 0:
+    print("Divisor :-",a/b)
+else:
+    print("Cannot be divided by Zero")
