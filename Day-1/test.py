7a4425bdf11a445846cc882df4e76062c06e6bbe
Revanth Kumar <revanth07315@gmail.com>
2025-06-30 22:53:23 +0530

    Learned Python basics – variables, input, conditionals

---
+++ Day-1/test.py
@@ -0,0 +1,6 @@
+name = input("What is your name :-")
+age = int(input("What is your age :-"))
+if age > 18:
+    print(f"Hi! {name},You are an Major")
+else:
+    print("Hi ",name +" You are a Minor" )
