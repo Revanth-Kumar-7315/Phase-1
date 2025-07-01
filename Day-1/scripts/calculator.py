a2c0539770826e976f3203b18ca8111271a8ab94
Revanth Kumar <revanth07315@gmail.com>
2025-07-01 16:53:12 +0530

    Finish Day 1: Add scripts and format with Black

--- Day-1/scripts/calculator.py
+++ Day-1/scripts/calculator.py
@@ -1,11 +1,11 @@
 a = int(input("Enter a number :"))
 b = int(input("Enter a number :"))
 
-print("Sum :-",a+b)
-print("Difference :-",a-b)
-print("Product :-",a*b)
+print("Sum :-", a + b)
+print("Difference :-", a - b)
+print("Product :-", a * b)
 
 if b != 0:
-    print("Divisor :-",a/b)
+    print("Divisor :-", a / b)
 else:
     print("Cannot be divided by Zero")
--- Day-1/test.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name :-")
-age = int(input("What is your age :-"))
-if age > 18:
-    print(f"Hi! {name},You are an Major")
-else:
-    print("Hi ",name +" You are a Minor" )
